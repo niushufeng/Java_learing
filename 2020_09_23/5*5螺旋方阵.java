4e3fdc4e13202b69cf4202d62e4f44c1d2fb838b
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 09:46:14 +0800

    Update 5*5螺旋方阵.java

--- 2020_09_23/5*5螺旋方阵.java
+++ 2020_09_23/5*5螺旋方阵.java
@@ -6,14 +6,14 @@ public class m02 {
 		// TODO Auto-generated method stub
 
 		int map[][] = new int[5][5];
-		 for(int i=0;i<5;i++)
-			 for(int j=0;j<5;j++)
-				 map[i][j]=0;
-		 int []x ={0,1,0,-1};
-		 int []y ={1,0,-1,0};
-		 int a=0,b=0,c=0;
-		 map[0][0]=1;
-		 for(int k=2;k<=25;k++)
+		 for(int i = 0;i < 5;i ++)
+			 for(int j = 0;j < 5;j ++)
+				 map[i][j] = 0;
+		 int []x = {0,1,0,-1};
+		 int []y = {1,0,-1,0};
+		 int a = 0,b = 0,c = 0;
+		 map[0][0] = 1;
+		 for(int k = 2;k <= 25;k ++)
 		 {
 			 int ta = a+x[c];
 			 int tb = b+y[c];
@@ -29,13 +29,13 @@ public class m02 {
 				 ta = a+x[c];
 				 tb = b+y[c];
 			 }
-			 a=ta;
-			 b=tb;
+			 a = ta;
+			 b = tb;
 			 map[a][b] = k;
 		 }
-		 for(int i=0;i<5;i++)
+		 for(int i = 0;i < 5;i ++)
 		 {
-			 for(int j=0;j<5;j++)
+			 for(int j = 0;j < 5; j++)
 			 {
 				 System.out.print(map[i][j]+" ");
 			 }
