2dcfbc752dd0f2198f504743febf8001e09c5875
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 08:55:48 +0800

    Update for循环.java

--- 2020_09_23/for循环.java
+++ 2020_09_23/for循环.java
@@ -5,7 +5,7 @@ public class m789 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int sum = 0;
-		for(int i=101;i<=200;i++) {
+		for(int i = 101;i <= 200;i ++) {
 			sum += i;
 	}
 		System.out.println("和是：" + sum);
