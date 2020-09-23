6ab5b406296586542736f50b1091b3df59625ec7
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 09:29:38 +0800

    Create 坐标转换.java

---
+++ 2020_09_23/坐标转换.java
@@ -0,0 +1,26 @@
+package m;
+
+class Point{
+	double x,y;
+	public void setXY(double a,double b){
+		x=a;
+		y=b;
+	}
+	public double getX(){
+		return x;
+	}
+	public double getY(){
+		return y;
+	}
+	public void disp(){
+		System.out.println("点的当前坐标为：("+x+", "+y+")");
+	}
+};
+public class javaTest{
+	public static void main(String[] args){
+		Point p1=new Point();
+		p1.disp();
+		p1.setXY(3.2,5.6);
+		p1.disp();
+	}
+}
