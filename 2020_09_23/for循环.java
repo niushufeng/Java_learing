5347bab1f776ad47faf63437bfdd3a07fe2e0a91
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 08:52:59 +0800

    Create for循环.java

---
+++ 2020_09_23/for循环.java
@@ -0,0 +1,13 @@
+package m;
+
+public class m789 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		for(int i=101;i<=200;i++) {
+			sum += i;
+	}
+		System.out.println("和是：" + sum);
+	}
+}
