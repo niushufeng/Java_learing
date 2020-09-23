c296f39c85e2de9e66f498b377b871fb590c486c
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 08:46:09 +0800

    Create while循环.java

---
+++ 2020_09_23/while循环.java
@@ -0,0 +1,14 @@
+package m;
+
+public class m123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0 ,i = 100;
+    	while(i < 200){
+    		i++;
+    		sum += i;
+	}
+    	System.out.println("和是：" + sum);
+	}
+}
