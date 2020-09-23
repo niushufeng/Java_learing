34b0aafd2a6830fae3041db9d4f9ce6301cde52b
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 09:28:39 +0800

    Create 运行判断字符.java

---
+++ 2020_09_23/运行判断字符.java
@@ -0,0 +1,24 @@
+package m;
+
+public class CharSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub //1831050017
+				char ch1 = '\b';
+			    char ch2 = '\t';
+			    char ch3 = '\n';
+			    char ch4 = '\r';
+			    char ch5 = '\"';
+			    char ch6 = '\'';
+			    char ch7 = '\\';
+		System.out.println("沈阳"+ch1+"师范大学");
+		System.out.println("沈阳"+ch2+"师范大学");
+		System.out.println("沈阳"+ch3+"师范大学");
+		System.out.println("沈阳"+ch4+ch3+"师范大学");
+		System.out.println(ch5+"沈阳"+"师范大学"+ch5);
+		System.out.println(ch6+"沈阳"+"师范大学"+ch6);
+		System.out.println(ch7+"沈阳"+"师范大学"+ch7);
+
+	}
+
+}
