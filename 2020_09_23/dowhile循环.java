8756574c97b8b4745c2437b2448b3026aa5b594b
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-09-23 08:50:24 +0800

    Create dowhile循环.java
    java使用不同语句计算大于100且小于等于200的数的和

---
+++ 2020_09_23/dowhile循环.java
@@ -0,0 +1,15 @@
+package m;
+
+public class m123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0,i=100;
+    	do{
+    		i++;
+    		sum += i;
+    	} while(i < 200);
+	
+    	System.out.println("和是：" + sum);
+	}
+}
