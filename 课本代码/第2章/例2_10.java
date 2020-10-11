250bea3ef60c7c88b73ea581836d4f67eb9c57d6
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-11 10:23:40 +0800

    Create 例2_10.java

---
+++ 课本代码/第2章/例2_10.java
@@ -0,0 +1,15 @@
+class BreakLoop 
+{
+	public static void main(String[] args) 
+	{
+		for (int i = 0 ; i < 100 ; i ++) 
+		{
+			if (i == 5)
+			{
+				break;
+			}
+			System.out.println("i:" + i);
+		}
+		System.out.println("Loop complete.");
+	}
+}
