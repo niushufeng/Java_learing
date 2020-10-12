a7ca5b2f4df014f14649219ed659350bcdb307d4
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-12 20:29:18 +0800

    Create 例3_3.java

---
+++ 课本代码/第3章/例3_3.java
@@ -0,0 +1,26 @@
+public class SumAll  
+{
+	public static void main(String[] args) 
+	{
+		int total = 0;
+		int arr[][] = new int[3][4];
+		for (int i = 0; i < arr.length ; i ++ )  // 初始化并显二维数组
+		{
+			for (int j = 0; j < arr.length ; j ++ )
+			{
+				arr[i][j] = i + j;
+				System.out.print(" " + arr[i][j]);
+			}
+			System.out.println("");			
+		}
+
+		for (int i = 0;i < arr.length ; i ++ )     //	求和
+		{
+			for (int j = 0; j < arr[i].length ; j ++)
+			{
+				total = total + arr[i][j];
+			}
+		}
+		System.out.println("The Sum is :" + total);
+	}
+}
