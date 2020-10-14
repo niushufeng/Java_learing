703c3ec75024c8bb42a9c66d1e7e873b90eb62a7
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-14 12:31:51 +0800

    Update Student.java

--- 2020_10_14/Student.java
+++ 2020_10_14/Student.java
@@ -4,73 +4,55 @@
  * 方法有“获得学号”、“获得班号”、“获得性别”、“获得姓名”、
  * “获得年龄”、“获得年龄”。
  * */
-package m101;
 
-public class Student1 {
-	private int Student_id;		//学号
-	private int class_id;	//班号
-	private String name;	// 姓名
+public class Student {
+	private int student_id;		//学号
+	private int class_id;		//班号
+	private String name;		//姓名
 	private boolean gender;		//性别
-	private int age;	//年龄
+	private int age;			//年龄
 	
-	Student1(int studet_id, int class_id, String name, boolean gender,int age){
-		
+	Student(int student_id, int class_id, String name, boolean gender,int age){
+		this.student_id = student_id;
+		this.class_id = class_id;
+		this.name = name;
+		this.gender = gender;
+		this.age = age;
 	}
+
+	public int getStudentId(){
+	   return student_id;
+	}
+	
+	public int getClassId(){
+	   return class_id;
+	}
+
+	public String getName(){
+	   return name;
+	}
+
+	public String getGender(){
+	   if (gender)
+	   {
+		   return "male";
+	   }else{ 
+			return "female";
+	   }
+	}
+
+	public int getAge(){
+	   return age;
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Student testMan = new Student(1831050017,1801,"luoxiang",false,45);
+		System.out.println(testMan.getStudentId());
+		System.out.println(testMan.getClassId());
+		System.out.println(testMan.getName());
+		System.out.println(testMan.getGender());
+		System.out.println(testMan.getAge());
 	}
 
 }
-/*
- * 
-package exam2;
- 
-import java.util.List;
- 
-public class Student {
-	// 定义一个学生类，Student有姓名，学号，选学的课程列表
-	private String stuname;
-	private String stuid;
-	private List<Course> courses;
- 
-	public Student() {
-	}
- 
-	public Student(String stuname, String stuid, List<Course> courses) {
-		this.stuname = stuname;
-		this.stuid = stuid;
-		this.courses = courses;
-	}
- 
-	public String getStuname() {
-		return stuname;
-	}
- 
-	public void setStuname(String stuname) {
-		this.stuname = stuname;
-	}
- 
-	public String getStuid() {
-		return stuid;
-	}
- 
-	public void setStuid(String stuid) {
-		this.stuid = stuid;
-	}
- 
-	public List<Course> getCourses() {
-		return courses;
-	}
- 
-	public void setCourses(List<Course> courses) {
-		this.courses = courses;
-	}
- 
-	@Override
-	public String toString() {
-		return "学生姓名=" + stuname + ",学号=" + stuid + ", 课程=" + courses;
-	}
- 
-}*/
- */
