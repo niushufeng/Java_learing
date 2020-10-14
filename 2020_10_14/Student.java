8ba02c518d4cb3a9e50a9e0e0f7f9edbd82570bd
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-14 09:40:38 +0800

    Create Student.java

---
+++ 2020_10_14/Student.java
@@ -0,0 +1,69 @@
+/*
+ * 1．编写一个Java程序片断，以定义一个表示学生的类Student。
+ * 这个类的属性有“学号”、“班号”、“姓名”、“性别”、“年龄”，
+ * 方法有“获得学号”、“获得班号”、“获得性别”、“获得姓名”、
+ * “获得年龄”、“获得年龄”。
+ * */
+
+package m101;
+
+public class Student1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+/*
+ * 
+package exam2;
+ 
+import java.util.List;
+ 
+public class Student {
+	// 定义一个学生类，Student有姓名，学号，选学的课程列表
+	private String stuname;
+	private String stuid;
+	private List<Course> courses;
+ 
+	public Student() {
+	}
+ 
+	public Student(String stuname, String stuid, List<Course> courses) {
+		this.stuname = stuname;
+		this.stuid = stuid;
+		this.courses = courses;
+	}
+ 
+	public String getStuname() {
+		return stuname;
+	}
+ 
+	public void setStuname(String stuname) {
+		this.stuname = stuname;
+	}
+ 
+	public String getStuid() {
+		return stuid;
+	}
+ 
+	public void setStuid(String stuid) {
+		this.stuid = stuid;
+	}
+ 
+	public List<Course> getCourses() {
+		return courses;
+	}
+ 
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+ 
+	@Override
+	public String toString() {
+		return "学生姓名=" + stuname + ",学号=" + stuid + ", 课程=" + courses;
+	}
+ 
+}*/
+ */
