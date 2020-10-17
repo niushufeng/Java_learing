4fc360e6c6b7cf1698e44bc1f2d3db2950d3ef40
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-17 21:44:52 +0800

    Create 例5_3.java

---
+++ 课本代码/第5章/例5_3.java
@@ -0,0 +1,39 @@
+//定义员工类
+class Employee
+{
+	private String name;
+	private int salary;
+	public String getDetails(){
+		return "Name :" + name  + "\nSalary: " + salary;
+	}
+	Employee(){
+	   name = "Tom";
+	   salary = 1234;
+	}
+}
+
+//定义经理类
+class Manager extends Employee
+{
+	public String department;
+	/*	重写getDetails方法	*/
+	public String getDetails(){
+	   System.out.println("I am in Manager");
+	   return super.getDetails();		//调用父类的getDetails方法
+	}
+	Manager(){
+	   super();		//访问父类的无参构造方法，即Employee()
+	   department = "sale";
+	}
+}
+
+
+public class Inheritance 
+{
+	public static void main(String[] args) 
+	{
+		Manager m = new Manager();
+		System.out.println(m.getDetails());
+		System.out.println("department:" + m.department);
+	}
+}
