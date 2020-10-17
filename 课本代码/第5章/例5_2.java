dc75de844d69360f89f51bc780c9aa146ec3a0db
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-10-17 21:44:04 +0800

    Create 例5_2.java

---
+++ 课本代码/第5章/例5_2.java
@@ -0,0 +1,15 @@
+class SuperCla
+{
+	int a = 3,b = 4;
+	void show(){
+	   System.out.println("super ");
+	}
+}
+
+public class OverrideTest 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+	}
+}
