70d0e1bcf19bf6586c81c1a6cbb7efa401a5e121
niushufeng <45967122+niushufeng@users.noreply.github.com>
2020-12-27 22:47:12 +0800

    Create code1.java

---
+++ tk/code1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+
+public class Java_1
+{
+    public static void main(String args[])
+    {
+     //*********Found********
+        final double TQ_IN=8.88;  //定义常量
+
+        byte   b;
+        int    i;
+     //*********Found********
+        char c;
+        double d=2.79994;
+
+        b=011;
+        i=234;
+        c='q';
+
+        System.out.println("TQ_IN= "+TQ_IN);
+        System.out.println("b= "+b);
+        System.out.println("i= "+i);
+        System.out.println("c= "+c);
+        System.out.println("d= "+d);
+    }
+}
